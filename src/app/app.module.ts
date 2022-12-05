0c6cd0fd3abf2f1798525e2154656d3ff5dce03e
alaoui omar <elalaouiomar1998@gmail.com>
2022-12-05 18:05:13 +0100

    seconde Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+
+import { BillComponent } from './bill/bill.component';
+import { BillDetailsComponent } from './bill-details/bill-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    BillComponent,
+    BillDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
